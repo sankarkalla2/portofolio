28e7649f563b4dcd5aebc0314feadc4fea9b8ec4
sankrkall2 <gowrisankarkalla4@gmail.com>
2024-11-08 11:53:43 +0530

    deploy ready

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true
-  }
+  },
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/sankar-portofolio', // Replace with your repository name
 };
 
 export default nextConfig;
